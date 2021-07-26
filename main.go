328e275cf9d8a498adcc851395b5207ec95191e2
nordmanii <thomas.nordmann@itslearning.com>
2021-07-26 22:23:49 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package itswizard_test
+
+import (
+	"fmt"
+	"./packages/pack1"
+)
+
+func main () {
+	fmt.Println("start")
+	pack1.Print()
+}
---
+++ packages/pack1/ff.go
@@ -0,0 +1,7 @@
+package pack1
+
+import "fmt"
+
+func Print () {
+	fmt.Println("12345")
+}
